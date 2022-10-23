b663668bc38df2bac796ccf5df401b3fb2c8b91a
lilianequipe <lilianequipe@gmail.com>
2022-10-23 13:48:51 -0300

    Exercicos de POO sobre Interface.

---
+++ POO-exercicios-interface/src/Calculadora.java
@@ -0,0 +1,22 @@
+public class Calculadora implements OperacaoMatematica {
+
+        @Override
+        public void soma(double numero1, double numero2) {
+
+        }
+
+        @Override
+        public void subtracao(double numero1, double numero2) {
+
+        }
+
+        @Override
+        public void multiplicacao(double numero1, double numero2) {
+
+        }
+
+        @Override
+        public void divisao(double numero1, double numero2) {
+
+        }
+}
